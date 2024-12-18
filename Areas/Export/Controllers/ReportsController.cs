9fb47b8868834bf0badfa4dd57a6fe6706848f11
Vishal Chudasama <vishal@padhyasoft.com>
2024-12-18 17:04:37 +0530

    Export : Report - MDA wise Pallate

--- Areas/Export/Controllers/ReportsController.cs
+++ Areas/Export/Controllers/ReportsController.cs
@@ -148,5 +148,94 @@ namespace Dispatch_System.Areas.Export.Controllers
                 return PartialView((searchTerm, result, isPrint));
         }
 
+
+        [HttpGet]
+        public IActionResult MDA_Wise_Pallate(string searchTerm = null, bool isPrint = false)
+        {
+            List<dynamic> result = new List<dynamic>();
+
+            DataSet ds = new DataSet();
+
+            if (!string.IsNullOrEmpty(searchTerm))
+                try
+                {
+                    List<MySqlParameter> oParams = new List<MySqlParameter>();
+
+                    oParams.Add(new MySqlParameter("P_ID", MySqlDbType.Int64) { Value = 0 });
+                    oParams.Add(new MySqlParameter("P_PALLATE_NO", MySqlDbType.VarString) { Value = "" });
+                    oParams.Add(new MySqlParameter("P_MDA_NO", MySqlDbType.VarString) { Value = searchTerm ?? "" });
+                    oParams.Add(new MySqlParameter("P_PLANT_ID", MySqlDbType.Int64) { Value = Common.Get_Session_Int(SessionKey.PLANT_ID) });
+
+                    ds = DataContext.ExecuteStoredProcedure_DataSet_SQL("PC_PALLATE_GET", oParams);
+
+                    if (ds != null && ds.Tables.Count > 0 && ds.Tables[0] != null && ds.Tables[0].Rows.Count > 0)
+                        foreach (DataRow dr in ds.Tables[0].Rows)
+                            try
+                            {
+                                result.Add(new
+                                {
+                                    Id = dr["ID"] != DBNull.Value ? Convert.ToInt64(dr["ID"]) : 0,
+                                    Sr_No = dr["Sr_No"] != DBNull.Value ? Convert.ToInt32(dr["Sr_No"]) : 0,
+                                    DI_No = dr["DI_No"] != DBNull.Value ? Convert.ToString(dr["DI_No"]) : "",
+                                    Pallate_Id = dr["Pallate_Id"] != DBNull.Value ? Convert.ToInt64(dr["Pallate_Id"]) : 0,
+                                    Pallate_No = dr["Pallate_No"] != DBNull.Value ? Convert.ToString(dr["Pallate_No"]) : "",
+                                    Pallate_Type = dr["Pallate_Type"] != DBNull.Value ? Convert.ToString(dr["Pallate_Type"]) : "",
+                                    Dispatch_Mode = dr["Dispatch_Mode"] != DBNull.Value ? Convert.ToString(dr["Dispatch_Mode"]) : "",
+                                    Pallate_Type_Text = dr["Pallate_Type_Text"] != DBNull.Value ? Convert.ToString(dr["Pallate_Type_Text"]) : "",
+                                    Dispatch_Mode_Text = dr["Dispatch_Mode_Text"] != DBNull.Value ? Convert.ToString(dr["Dispatch_Mode_Text"]) : "",
+                                    Plant_Name = dr["PLANT_NAME"] != DBNull.Value ? Convert.ToString(dr["PLANT_NAME"]) : "",
+                                    Plant_Address = dr["PLANT_ADDRESS"] != DBNull.Value ? Convert.ToString(dr["PLANT_ADDRESS"]) : "",
+                                    Report_Title = dr["REPORT_TITLE"] != DBNull.Value ? Convert.ToString(dr["REPORT_TITLE"]) : "",
+                                    Vehicle_No = dr["VEHICLE_NO"] != DBNull.Value ? Convert.ToString(dr["VEHICLE_NO"]) : "",
+                                    MDA_No = dr["MDA_NO"] != DBNull.Value ? Convert.ToString(dr["MDA_NO"]) : "",
+                                    Gate_In_dt = dr["GATE_IN_DT"] != DBNull.Value ? Convert.ToString(dr["GATE_IN_DT"]) : "",
+                                    Gate_Out_dt = dr["GATE_OUT_DT"] != DBNull.Value ? Convert.ToString(dr["GATE_OUT_DT"]) : "",
+                                    Transporter_Name = dr["TRANS_NAME"] != DBNull.Value ? Convert.ToString(dr["TRANS_NAME"]) : null,
+                                    Driver_Name = dr["DRIVER_NAME"] != DBNull.Value ? Convert.ToString(dr["DRIVER_NAME"]) : null,
+                                    Driver_Contact = dr["DRIVER_CONTACT"] != DBNull.Value ? Convert.ToString(dr["DRIVER_CONTACT"]) : null,
+                                    Expected_Shipper = dr["Expected_Shipper"] != DBNull.Value ? Convert.ToDecimal(dr["Expected_Shipper"]) : 0,
+                                    Loaded_Shipper = dr["Loaded_Shipper"] != DBNull.Value ? Convert.ToDecimal(dr["Loaded_Shipper"]) : 0,
+                                    Required_Shipper = dr["Required_Shipper"] != DBNull.Value ? Convert.ToDecimal(dr["Required_Shipper"]) : 0,
+                                    Party_Name = dr["PARTY_NAME"] != DBNull.Value ? Convert.ToString(dr["PARTY_NAME"]) : "",
+                                    Dist = dr["DIST"] != DBNull.Value ? Convert.ToInt64(dr["DIST"]) : 0,
+                                    Desp_Place = dr["DESP_PLACE"] != DBNull.Value ? Convert.ToString(dr["DESP_PLACE"]) : "",
+                                    Shipper_QR_Code = new List<Pallate_Shipper>()
+                                });
+                            }
+                            catch { continue; }
+
+                    if (ds != null && ds.Tables.Count > 1 && ds.Tables[1] != null && ds.Tables[1].Rows.Count > 0)
+                        foreach (DataRow dr in ds.Tables[1].Rows)
+                        {
+                            try
+                            {
+                                var index = result.FindIndex(x => x.Pallate_Id == (dr["Pallate_Id"] != DBNull.Value ? Convert.ToInt64(dr["Pallate_Id"]) : 0));
+
+                                if (index >= 0)
+                                {
+                                    if (result[index].Shipper_QR_Code == null)
+                                        result[index].Shipper_QR_Code = new List<Pallate_Shipper>();
+
+                                    result[index].Shipper_QR_Code.Add(new Pallate_Shipper()
+                                    {
+                                        Id = dr["ID"] != DBNull.Value ? Convert.ToInt64(dr["ID"]) : 0,
+                                        Sr_No = dr["Sr_No"] != DBNull.Value ? Convert.ToInt32(dr["Sr_No"]) : 0,
+                                        Pallate_Id = dr["Pallate_Id"] != DBNull.Value ? Convert.ToInt64(dr["Pallate_Id"]) : 0,
+                                        QR_Code = dr["Shipper_QR_Code"] != DBNull.Value ? Convert.ToString(dr["Shipper_QR_Code"]) : ""
+                                    });
+                                }
+                            }
+                            catch { continue; }
+                        }
+
+                }
+                catch (Exception ex) { LogService.LogInsert(GetCurrentAction(), "", ex); }
+
+            if (isPrint == true || string.IsNullOrEmpty(searchTerm))
+                return View((searchTerm, result, isPrint));
+            else
+                return PartialView((searchTerm, result, isPrint));
+        }
+
     }
 }
